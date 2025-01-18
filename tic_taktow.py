e225b246f089e741b878e8c62d49960c5ced1432
vinukond <102307805+vinukond@users.noreply.github.com>
2025-01-18 20:36:27 +0000

    To check git working sample code

--- tic_taktow.py
+++ tic_taktow.py
@@ -1,2 +1,15 @@
-print("hello")
-print("world")
+dict = {'1':"monitor",'2':"Mouse",'3':"Keyboard",'4':"CPU",'5':"Disk"}
+
+l = []
+
+
+choice = input("> ")
+while choice !=0:
+    if choice in dict:
+        l.append(dict[choice])
+    elif choice == 'ok':
+        print(l)
+        break
+    else:
+        print(dict)
+    choice = input("> ")
